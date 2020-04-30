f4b99902a322a04aea947f0aca8a601cc45c8b8d
用户名 <1234@.com>
2020-04-30 21:15:21 +0800

    周怡珊修改了

--- src/main/java/com/newer/dao/AttendanceDaoMapper.java
+++ src/main/java/com/newer/dao/AttendanceDaoMapper.java
@@ -1,7 +1,13 @@
 package com.newer.dao;
 
 import com.newer.domain.Attendance;
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface AttendanceDaoMapper extends Mapper <Attendance> {
+
+    @Select("select a.*,b.name from t_eave a,users b where a.userid=b.id")
+    public List<Attendance> findAll();
 }
--- src/main/java/com/newer/service/AttendanceService.java
+++ src/main/java/com/newer/service/AttendanceService.java
@@ -2,16 +2,21 @@ package com.newer.service;
 
 import com.github.pagehelper.PageInfo;
 import com.newer.domain.Attendance;
+import com.newer.dto.PageDto;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
 
 /**
  * 考勤表模块
  * 周怡珊
  */
-public interface AttendanceService extends Mapper <Attendance> {
+public interface AttendanceService  {
     public boolean sava(Attendance attendance);
     public boolean delete(Integer id);
     public boolean update(Attendance attendance);
-
+    public List<Attendance> findAll() ;
+    public PageInfo<Attendance> findAll(Integer page, PageDto pageDto);
     public Attendance findAttendancebyid(Integer id);
 }
